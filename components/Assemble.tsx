4d602e54aa93db9e63a519c2d7fd4fb4ae742c1c
tarifalhasan <tarifalhasan999@gmail.com>
2024-03-19 14:28:00 +0600

    Not Just a Meme Coin,

--- components/Assemble.tsx
+++ components/Assemble.tsx
@@ -71,8 +71,40 @@ const Assemble = () => {
           backgroundImage: `url("/shape-2.png")`,
           backgroundSize: "100%",
         }}
-        className=" mt-[-6%] 2xl:!bg-contain  w-full  z-40 relative  overflow-hidden pb-[10%] bg-no-repeat "
+        className=" mt-[-6%]   w-full  z-40 relative  overflow-hidden pb-[10%] bg-no-repeat "
       ></div>
+      <div className=" relative z-20 pt-24  container flex flex-col lg:flex-row  lg:items-center gap-8">
+        <div className=" order-first  basis-full lg:basis-1/2">
+          <div className="">
+            <Image
+              width={425}
+              height={400}
+              className=" w-full h-full object-cover"
+              src={"/meme-coin.webp"}
+              alt="hero"
+            />
+          </div>
+        </div>
+        <div className=" basis-full  space-y-2 lg:-space-y-6 xl:-space-y-8 lg:basis-1/2">
+          <div className=" ">
+            <div>
+              <h4 className=" leading-[110%] tracking-[1.44px] text-5xl lg:text-5xl xl:text-6xl  text-skin-gray-900 font-n27 font-normal">
+                <span className="  ml-[10%]"> $SOSA: </span>
+                <span className=" flex  gap-4 ">Not Just a Meme Coin,</span>
+              </h4>
+            </div>
+            <div className=" lg:pt-10   space-y-6  block  mx-auto max-w-[301px] ">
+              <h4 className=" text-xl xl:text-2xl font-normal font-geliat">
+                a Declaration of War on Crypto&apos;s Centralization.
+              </h4>
+              <p className=" text-base font-normal text-skin-gray-800 font-geliat">
+                We&apos;re Seizing Power, Charting the Exodus to Financial
+                Freedom.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
